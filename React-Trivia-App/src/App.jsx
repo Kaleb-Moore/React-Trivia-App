186e36ec1b7a194e0ecb7c25cec1c53f6afa6687
Kaleb Moore <k4l3bmoore@gmail.com>
2022-08-22 14:55:39 -0600

    Create Button Component

--- React-Trivia-App/src/App.jsx
+++ React-Trivia-App/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Button from './Button.jsx';
 
 export default function App() {
     return (
         <div>
             <p>Hello World!</p>
+            <Button value="Start Quiz"/>
         </div>
     )
 }
