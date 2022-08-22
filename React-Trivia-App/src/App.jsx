5b1f36cc09c94830b11a4b524fd476c8314f8822
Kaleb Moore <k4l3bmoore@gmail.com>
2022-08-22 17:09:45 -0600

    Create Check Answers button

--- React-Trivia-App/src/App.jsx
+++ React-Trivia-App/src/App.jsx
@@ -43,6 +43,7 @@ export default function App() {
                 :
                 <div>
                     {cards}
+                    <Button value="Check answers" />
                 </div>
             }
         </div>
